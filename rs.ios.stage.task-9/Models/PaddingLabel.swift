6d24faebc939bb9a5fd84f513200e6070d213f61
Maksim Karaseu <dassive_mick@mail.ru>
2021-08-04 22:49:01 +0200

    fear: gallery collection, settings controller

---
+++ rs.ios.stage.task-9/Models/PaddingLabel.swift
@@ -0,0 +1,20 @@
+//
+//  PaddingLabel.swift
+//  rs.ios.stage.task-9
+//
+//  Created by dev on 4.08.21.
+//
+
+import UIKit
+
+class PaddingLabel: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ rs.ios.stage.task-9/Models/TypeView.swift
@@ -0,0 +1,20 @@
+//
+//  TypeView.swift
+//  rs.ios.stage.task-9
+//
+//  Created by dev on 4.08.21.
+//
+
+import UIKit
+
+class TypeView: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
